63667e8ff7504acf364dd4f8690e7b06b82d3ad0
Vadim Bilan <71188139+vadimb892@users.noreply.github.com>
2024-06-13 13:17:59 +0300

    Update state.rs

--- crates/bevy_winit/src/state.rs
+++ crates/bevy_winit/src/state.rs
@@ -41,6 +41,9 @@ use crate::{
     WinitSettings, WinitWindows,
 };
 
+/// Set this flag to `true` to prohibit bevy updates
+pub static PAUSE: std::sync::atomic::AtomicBool = std::sync::atomic::AtomicBool::new(false);
+
 /// Persistent state that is used to run the [`App`] according to the current
 /// [`UpdateMode`].
 struct WinitAppRunnerState<T: Event> {
@@ -384,6 +387,10 @@ impl<T: Event> ApplicationHandler<T> for WinitAppRunnerState<T> {
     }
 
     fn about_to_wait(&mut self, event_loop: &ActiveEventLoop) {
+        if PAUSE.load(std::sync::atomic::Ordering::Acquire){
+            self.lifecycle = AppLifecycle::Suspend;            
+        }
+                                     
         // create any new windows
         // (even if app did not update, some may have been created by plugin setup)
         let mut create_window =
